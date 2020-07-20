dc0accb0ecf563e266a4b4d32899f734de7d1645
solid <solid@DESKTOP-N491H8S>
2020-07-20 04:58:43 -0500

    Agregar archivos de proyecto.

---
+++ AllOver/Controllers/HomeController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using AllOver.Models;
+namespace AllOver.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
+        {
+        }
+
+        public HomeController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        [AllowAnonymous]
+        [Authorize(Roles = "Cliente")]
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                var manager = new UserManager<AllOver.Models.ApplicationUser>(new UserStore<AllOver.Models.ApplicationUser>(new AllOver.Models.ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                var nombre = currentUser.FistName;
+                var apellido = currentUser.LastName;
+                ViewBag.FistName = nombre;
+                ViewBag.LastName = apellido;
+            }
+
+            return View();
+        }
+        [AllowAnonymous]
+        [Authorize(Roles = "Cliente")]
+        public ActionResult About()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                var manager = new UserManager<AllOver.Models.ApplicationUser>(new UserStore<AllOver.Models.ApplicationUser>(new AllOver.Models.ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                var nombre = currentUser.FistName;
+                var apellido = currentUser.LastName;
+                ViewBag.FistName = nombre;
+                ViewBag.LastName = apellido;
+            }
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [ AllowAnonymous]
+        [Authorize(Roles = "Cliente")]
+        public ActionResult Catalogo()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                var manager = new UserManager<AllOver.Models.ApplicationUser>(new UserStore<AllOver.Models.ApplicationUser>(new AllOver.Models.ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                var nombre = currentUser.FistName;
+                var apellido = currentUser.LastName;
+                ViewBag.FistName = nombre;
+                ViewBag.LastName = apellido;
+            }
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Cliente")]
+        public ActionResult Tiendasfisicas()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                var manager = new UserManager<AllOver.Models.ApplicationUser>(new UserStore<AllOver.Models.ApplicationUser>(new AllOver.Models.ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                var nombre = currentUser.FistName;
+                var apellido = currentUser.LastName;
+                ViewBag.FistName = nombre;
+                ViewBag.LastName = apellido;
+            }
+
+            return View();
+        }
+
+        [Authorize(Roles = "Cliente")]
+        public ActionResult Tiendasonline()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                var manager = new UserManager<AllOver.Models.ApplicationUser>(new UserStore<AllOver.Models.ApplicationUser>(new AllOver.Models.ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                var nombre = currentUser.FistName;
+                var apellido = currentUser.LastName;
+                ViewBag.FistName = nombre;
+                ViewBag.LastName = apellido;
+            }
+
+            return View();
+        }
+        [AllowAnonymous]
+       
+        public ActionResult Producto()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var currentUserId = User.Identity.GetUserId();
+                var manager = new UserManager<AllOver.Models.ApplicationUser>(new UserStore<AllOver.Models.ApplicationUser>(new AllOver.Models.ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                var nombre = currentUser.FistName;
+                var apellido = currentUser.LastName;
+                ViewBag.FistName = nombre;
+                ViewBag.LastName = apellido;
+            }
+
+            return View();
+        }
+
+    }
+
+}
+
+    
---
+++ AllOver/Controllers/RolesAdminController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using AllOver.Models;
+
+namespace AllOver.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RolesAdminController : Controller
+    {
+        public RolesAdminController()
+        {
+        }
+
+        public RolesAdminController(ApplicationUserManager userManager,
+            ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
+        private ApplicationRoleManager _roleManager;
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+
+
+
+        // GET: RolesAdmin
+        public ActionResult Index()
+        {
+            return View(RoleManager.Roles);
+        }
+
+        // GET: RolesAdmin/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var role = await RoleManager.FindByIdAsync(id);
+            var users = new List<ApplicationUser>();
+            foreach (var user in UserManager.Users.ToList())
+            {
+                if (await UserManager.IsInRoleAsync(user.Id, role.Name))
+                {
+                    users.Add(user);
+                }
+            }
+
+            ViewBag.Users = users;
+            ViewBag.UserCount = users.Count();
+            return View(role);
+        }
+    
+
+        // GET: RolesAdmin/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RolesAdmin/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] RoleViewModel roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole(roleViewModel.Name);
+                var roleresult = await RoleManager.CreateAsync(role);
+                if (!roleresult.Succeeded)
+                {
+                    ModelState.AddModelError("", roleresult.Errors.First());
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // GET: RolesAdmin/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            RoleViewModel roleModel = new RoleViewModel { Id = role.Id, Name = role.Name };
+            return View(roleModel);
+        }
+
+        // POST: RolesAdmin/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] RoleViewModel roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await RoleManager.FindByIdAsync(roleViewModel.Id);
+                role.Name = roleViewModel.Name;
+                await RoleManager.UpdateAsync(role);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: RolesAdmin/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: RolesAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id,string deleteUser)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                var role = await RoleManager.FindByIdAsync(id);
+                if (role == null)
+                {
+                    return HttpNotFound();
+                }
+                IdentityResult result;
+                if (deleteUser != null)
+                {
+                    result = await RoleManager.DeleteAsync(role);
+                }
+                else
+                {
+                    result = await RoleManager.DeleteAsync(role);
+                }
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", result.Errors.First());
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                RoleManager.Dispose();
+                UserManager.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
